a16ab69fa910ecc991b3115a0e6aa3d63db222c3
Vita Dolejsi <vitek.dolejsi@seznam.cz>
2023-12-06 11:33:34 +0100

    polished code a bit

--- epptec_noticeboard/src/components/post_page/postPage.js
+++ epptec_noticeboard/src/components/post_page/postPage.js
@@ -24,19 +24,30 @@ const PostPage = () => {
         <Link to="/" className="text-blue-600 hover:text-blue-800">
           Back to the Notice Board
         </Link>
-        <h1 className="text-xl font-bold my-2">{post.title}</h1>
-        <p className="text-l mb-4">{post.author}</p>
-        <p className="text-sm my-4">{post.body}</p>
-        <h2 className="text-lg font-bold mt-6 mb-2">Comments</h2>
-        <div className="flex flex-col items-start">
-          {post.comments.map((comment) => (
-            <Comment
-              title={comment.name}
-              email={comment.email}
-              body={comment.body}
-            />
-          ))}
-        </div>
+        {post.title && post.author && post.body ? (
+          <>
+            <h1 className="text-xl font-bold my-2">{post.title}</h1>
+            <p className="text-l mb-4">{post.author}</p>
+            <p className="text-sm my-4">{post.body}</p>
+            <h2 className="text-lg font-bold mt-6 mb-2">Comments</h2>
+            <div className="flex flex-col items-start">
+              {post.comments.map((comment) => (
+                <Comment
+                  key={comment.id} // Assuming each comment has a unique ID
+                  title={comment.name}
+                  email={comment.email}
+                  body={comment.body}
+                />
+              ))}
+            </div>
+          </>
+        ) : (
+          <>
+            <h1 className="text-xl font-bold my-2">
+              Sorry, no such post exists in our database.
+            </h1>
+          </>
+        )}
       </div>
     </div>
   );
